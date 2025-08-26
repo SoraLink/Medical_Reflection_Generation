c2f4202138c2ad24571e7daa2459b3c8c193263b
Soralink <qitianye1104@gmail.com>
2025-08-26 22:40:43 +1000

    change path

--- download_histai.py
+++ download_histai.py
@@ -1,12 +1,5 @@
 from huggingface_hub import snapshot_download
 
-# 下载混合集
-mix_dir = snapshot_download(
-    repo_id="histai/HISTAI-mixed",
-    repo_type="dataset",
-    local_dir="./HISTAI/HISTAI-mixed",
-)
-
 # 下载元数据（包含 diagnosis / conclusion / icd10 等，并给出 case 到图像的映射）
 meta_dir = snapshot_download(
     repo_id="histai/HISTAI-metadata",
