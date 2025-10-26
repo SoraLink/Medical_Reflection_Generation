5d8ee55922d9f975e7338aca390029720890693d
Soralink <qitianye1104@gmail.com>
2025-10-26 17:18:12 +1100

    evaluate Imagen

--- models.py
+++ models.py
@@ -331,9 +331,18 @@ class ImagenModel:
             cond_drop_prob=0.1,
             timesteps=50
         ).cuda()
+        self.trainer = ImagenTrainer(
+            imagen,
+            use_ema=True,
+            lr=1e-4,
+            warmup_steps=500,
+            checkpoint_path='./imagen_LIDC',
+            checkpoint_every=200,
+            max_grad_norm=1.0,
+            split_valid_from_train=False,
+        )
         try:
-            print(f"Loading model from {model_path}")
-            self.imagen = load_imagen_from_checkpoint(model_path).to(self.device)
+            self.trainer.load_from_checkpoint_folder()
         except Exception:
             # 兜底：若没有打包的checkpoint，就用手动方式加载
             ckpt = torch.load(model_path, map_location=self.device)
@@ -345,7 +354,7 @@ class ImagenModel:
     @torch.no_grad()
     def __call__(self, prompts, num_inference_steps, stop_at_unet_number: int = 3, cond_scale: float = 3.0,
                  return_pil_images: bool = True):
-        images = self.imagen.sample(
+        images = self.trainer.sample(
             texts=prompts,
             batch_size=len(prompts),
             return_pil_images=return_pil_images,
